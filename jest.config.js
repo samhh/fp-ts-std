bd09b707bdf61a0134400e4f27d75543234da0bb
Sam A. Horvath-Hunt <hello@samhh.com>
2020-10-20 23:33:45 +0100

    Report coverage with Jest
    The current thresholds are the present level of coverage. These should only be increased until we're at 100% across the board.

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,15 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testRegex: '\/test\/',
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            branches: 0,
+            functions: 50,
+            lines: 92,
+            statements: 73,
+        },
+    },
     globals: {
         'ts-jest': {
             diagnostics: false,
