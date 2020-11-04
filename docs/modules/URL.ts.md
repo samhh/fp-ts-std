1f753e264449819ea01d78a1de5acaef4ae3d7d1
Sam A. Horvath-Hunt <hello@samhh.com>
2020-11-04 23:37:54 +0000

    Add examples to all URL fns

--- docs/modules/URL.ts.md
+++ docs/modules/URL.ts.md
@@ -32,6 +32,15 @@ Refine a foreign value to `URL`.
 export declare const isURL: Refinement<unknown, URL>
 ```
 
+**Example**
+
+```ts
+import { isURL } from 'fp-ts-std/URL'
+
+assert.strictEqual(isURL(new URL('https://samhh.com')), true)
+assert.strictEqual(isURL({ not: { a: 'url' } }), false)
+```
+
 Added in v0.1.0
 
 ## parse
@@ -42,6 +51,19 @@ Safely parse a `URL`.
 
 ```ts
 export declare const parse: <E>(f: (e: TypeError) => E) => (x: string) => Either<E, URL>
+```
+
+**Example**
+
+```ts
+import { parse } from 'fp-ts-std/URL'
+import * as E from 'fp-ts/Either'
+import { constant } from 'fp-ts/function'
+
+const f = parse(constant('e'))
+
+assert.deepStrictEqual(f('https://samhh.com'), E.right(new URL('https://samhh.com')))
+assert.deepStrictEqual(f('invalid'), E.left('e'))
 ```
 
 Added in v0.1.0
@@ -56,6 +78,16 @@ Safely parse a `URL`, returning an `Option`.
 export declare const parseO: (href: string) => Option<URL>
 ```
 
+**Example**
+
+```ts
+import { parseO } from 'fp-ts-std/URL'
+import * as O from 'fp-ts/Option'
+
+assert.deepStrictEqual(parseO('https://samhh.com'), O.some(new URL('https://samhh.com')))
+assert.deepStrictEqual(parseO('invalid'), O.none)
+```
+
 Added in v0.1.0
 
 ## unsafeParse
@@ -66,6 +98,14 @@ Unsafely parse a `URL`, throwing on failure.
 
 ```ts
 export declare const unsafeParse: (x: string) => URL
+```
+
+**Example**
+
+```ts
+import { unsafeParse } from 'fp-ts-std/URL'
+
+assert.deepStrictEqual(unsafeParse('https://samhh.com'), new URL('https://samhh.com'))
 ```
 
 Added in v0.1.0
--- src/URL.ts
+++ src/URL.ts
@@ -11,12 +11,27 @@ import { flow, identity, pipe, Refinement } from 'fp-ts/function';
 /**
  * Unsafely parse a `URL`, throwing on failure.
  *
+ * @example
+ * import { unsafeParse } from 'fp-ts-std/URL';
+ *
+ * assert.deepStrictEqual(unsafeParse('https://samhh.com'), new URL('https://samhh.com'));
+ *
  * @since 0.1.0
  */
 export const unsafeParse = (x: string): URL => new URL(x);
 
 /**
  * Safely parse a `URL`.
+ *
+ * @example
+ * import { parse } from 'fp-ts-std/URL';
+ * import * as E from 'fp-ts/Either';
+ * import { constant } from 'fp-ts/function';
+ *
+ * const f = parse(constant('e'));
+ *
+ * assert.deepStrictEqual(f('https://samhh.com'), E.right(new URL('https://samhh.com')));
+ * assert.deepStrictEqual(f('invalid'), E.left('e'));
  *
  * @since 0.1.0
  */
@@ -29,6 +44,13 @@ export const parse = <E>(f: (e: TypeError) => E) => (x: string): Either<E, URL> 
 /**
  * Safely parse a `URL`, returning an `Option`.
  *
+ * @example
+ * import { parseO } from 'fp-ts-std/URL';
+ * import * as O from 'fp-ts/Option';
+ *
+ * assert.deepStrictEqual(parseO('https://samhh.com'), O.some(new URL('https://samhh.com')));
+ * assert.deepStrictEqual(parseO('invalid'), O.none);
+ *
  * @since 0.1.0
  */
 export const parseO: (href: string) => Option<URL> =
@@ -36,6 +58,12 @@ export const parseO: (href: string) => Option<URL> =
 
 /**
  * Refine a foreign value to `URL`.
+ *
+ * @example
+ * import { isURL } from 'fp-ts-std/URL';
+ *
+ * assert.strictEqual(isURL(new URL('https://samhh.com')), true);
+ * assert.strictEqual(isURL({ not: { a: 'url' } }), false);
  *
  * @since 0.1.0
  */
