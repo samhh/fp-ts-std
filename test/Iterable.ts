018d7e3821094244cc7c894199cd109918a7f737
Wayne Van Son <waynevanson@gmail.com>
2021-10-17 16:19:19 +1100

    test(Iterable): wrap in typeclass block

--- test/Iterable.ts
+++ test/Iterable.ts
@@ -4,11 +4,13 @@ import * as fc from "fast-check"
 import * as laws from "fp-ts-laws"
 
 describe("iterable", () => {
-  describe("getEq", () => {
-    it("should equal itself when empty", () => {
-      const eq = iterable.getEq(String.Eq)
-      // eslint-disable-next-line
-      laws.eq(eq, fc.array(fc.string()))
+  describe("typeclasses", () => {
+    describe("getEq", () => {
+      it("should equal itself when empty", () => {
+        const eq = iterable.getEq(String.Eq)
+        // eslint-disable-next-line
+        laws.eq(eq, fc.array(fc.string()))
+      })
     })
   })
 
