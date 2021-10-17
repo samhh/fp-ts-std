4d0e2c31caea0f470b025aab9880e0084c2ccae6
Wayne Van Son <waynevanson@gmail.com>
2021-10-17 21:30:07 +1100

    feat(Iterable): huge thicc changes

--- src/Iterable.ts
+++ src/Iterable.ts
@@ -1,7 +1,21 @@
 /**
  * @since 0.12.0
  */
-import { eq as EQ } from "fp-ts"
+import { either as E, eq as EQ, option as O, readonlyArray as A } from "fp-ts"
+import { Applicative1 } from "fp-ts/lib/Applicative"
+import { Apply1 } from "fp-ts/lib/Apply"
+import { Chain1 } from "fp-ts/lib/Chain"
+import { Compactable1 } from "fp-ts/lib/Compactable"
+import {
+  FilterableWithIndex1,
+  PredicateWithIndex,
+  RefinementWithIndex,
+} from "fp-ts/lib/FilterableWithIndex"
+import { Foldable } from "fp-ts/lib/Foldable"
+import { FoldableWithIndex1 } from "fp-ts/lib/FoldableWithIndex"
+import { pipe } from "fp-ts/lib/function"
+import { FunctorWithIndex1 } from "fp-ts/lib/FunctorWithIndex"
+import { Monad1 } from "fp-ts/lib/Monad"
 import { Pointed1 } from "fp-ts/lib/Pointed"
 
 /**
@@ -66,10 +80,232 @@ export function getEq<A>(eq: EQ.Eq<A>): EQ.Eq<Iterable<A>> {
 }
 
 /**
+ * Takes an object that contains the iterable protocol anc coerces it into an
+ * iterable.
+ *
+ * @category Interop
+ * @since 0.12.0
+ */
+export function fromIterable<A, T extends Iterable<A>>(fa: T): Iterable<A> {
+  return fa
+}
+
+/**
+ * Loads all of the elements into memory by placing them all into a
+ * `ReadonlyArray`.
+ *
+ * @category Destructors
+ * @since 0.12.0
+ */
+export function toReadonlyArray<A>(fa: Iterable<A>): ReadonlyArray<A> {
+  return Array.from(fa)
+}
+
+/**
+ * @category FunctorWithIndex
+ * @since 2.0.0
+ */
+export const mapWithIndex: <A, B>(
+  f: (i: number, a: A) => B,
+) => (fa: Iterable<A>) => Iterable<B> = f => fa => ({
+  *[Symbol.iterator]() {
+    // eslint-disable-next-line functional/no-let
+    let i = 0
+    // eslint-disable-next-line functional/no-loop-statement
+    for (const a of fa) {
+      // eslint-disable-next-line functional/no-expression-statement
+      yield f(i++, a)
+    }
+  },
+})
+
+/**
+ * `map` can be used to turn functions `(a: A) => B` into functions `(fa: F<A>) => F<B>` whose argument and return types
+ * use the type constructor `F` to represent some computational context.
+ *
+ * @category Functor
+ * @since 0.12.0
+ */
+export const map: <A, B>(f: (a: A) => B) => (fa: Iterable<A>) => Iterable<B> =
+  f => mapWithIndex((i, a) => f(a))
+
+/**
  * @category instances
  * @since 0.12.0
  */
 export const Pointed: Pointed1<URI> = {
   URI,
   of,
+}
+
+/**
+ * @category instances
+ * @since 2.7.0
+ */
+export const FunctorWithIndex: FunctorWithIndex1<URI, number> = {
+  URI,
+  mapWithIndex: (fa, f) => mapWithIndex(f)(fa),
+  map: (fa, f) => map(f)(fa),
+}
+
+export const Apply: Apply1<URI> = {
+  ...FunctorWithIndex,
+  ap: (fab, fa) => ({
+    *[Symbol.iterator]() {
+      // eslint-disable-next-line functional/no-loop-statement
+      for (const f of fab) {
+        // eslint-disable-next-line functional/no-loop-statement
+        for (const a of fa) {
+          // eslint-disable-next-line functional/no-expression-statement
+          yield f(a)
+        }
+      }
+    },
+  }),
+}
+
+export const Applicative: Applicative1<URI> = {
+  ...Apply,
+  ...Pointed,
+}
+
+export const Chain: Chain1<URI> = {
+  ...Apply,
+  chain: (fa, f) => ({
+    *[Symbol.iterator]() {
+      // eslint-disable-next-line functional/no-loop-statement
+      for (const a of fa) {
+        // eslint-disable-next-line functional/no-loop-statement
+        for (const b of f(a)) {
+          // eslint-disable-next-line functional/no-expression-statement
+          yield b
+        }
+      }
+    },
+  }),
+}
+
+export const Monad: Monad1<URI> = {
+  ...Pointed,
+  ...Chain,
+}
+
+const _filterMapWithIndex: FilterableWithIndex1<
+  URI,
+  number
+>["filterMapWithIndex"] = (fa, f) => ({
+  *[Symbol.iterator]() {
+    // eslint-disable-next-line functional/no-let
+    let i = 0
+    // eslint-disable-next-line functional/no-loop-statement
+    for (const a of fa) {
+      const ob = f(i++, a)
+      // eslint-disable-next-line functional/no-conditional-statement
+      if (O.isSome(ob)) {
+        //eslint-disable-next-line functional/no-expression-statement
+        yield ob.value
+      }
+    }
+  },
+})
+
+const _partitionMapWithIndex: FilterableWithIndex1<
+  URI,
+  number
+>["partitionMapWithIndex"] = (fa, f) => ({
+  left: _filterMapWithIndex(fa, (i, a) => pipe(f(i, a), E.swap, O.fromEither)),
+  right: _filterMapWithIndex(fa, (i, a) => pipe(f(i, a), O.fromEither)),
+})
+
+export const Compactable: Compactable1<URI> = {
+  URI,
+  compact: fa => _filterMapWithIndex(fa, (i, a) => a),
+  separate: fa => _partitionMapWithIndex(fa, (i, a) => a),
+}
+
+export const FilterableWithIndex: FilterableWithIndex1<URI, number> = {
+  ...Compactable,
+  ...FunctorWithIndex,
+  filter: <A, B extends A>(
+    fa: Iterable<A>,
+    f: PredicateWithIndex<number, A> | RefinementWithIndex<number, A, B>,
+  ) =>
+    _filterMapWithIndex(fa, (i, a) =>
+      pipe(
+        a,
+        O.fromPredicate(a => f(i, a)),
+      ),
+    ),
+  filterWithIndex: <A, B extends A>(
+    fa: Iterable<A>,
+    f: PredicateWithIndex<number, A> | RefinementWithIndex<number, A, B>,
+  ): Iterable<B> =>
+    _filterMapWithIndex(fa, (i, a) =>
+      pipe(
+        a,
+        O.fromPredicate((a): a is B => f(i, a)),
+      ),
+    ),
+  filterMap: (fa, f) => _filterMapWithIndex(fa, (i, a) => f(a)),
+  filterMapWithIndex: _filterMapWithIndex,
+  partitionMapWithIndex: _partitionMapWithIndex,
+  partitionMap: (fa, f) => _partitionMapWithIndex(fa, (i, a) => f(a)),
+  partitionWithIndex: <A, B extends A>(
+    fa: Iterable<A>,
+    f: PredicateWithIndex<number, A> | RefinementWithIndex<number, A, B>,
+  ) =>
+    _partitionMapWithIndex(fa, (i, a) =>
+      pipe(
+        a,
+        E.fromPredicate(
+          a => f(i, a),
+          e => e,
+        ),
+      ),
+    ),
+  partition: <A, B extends A>(
+    fa: Iterable<A>,
+    f: PredicateWithIndex<number, A> | RefinementWithIndex<number, A, B>,
+  ) =>
+    _partitionMapWithIndex(fa, (i, a) =>
+      pipe(
+        a,
+        E.fromPredicate(
+          a => f(i, a),
+          e => e,
+        ),
+      ),
+    ),
+}
+
+const _reduceWithIndex: FoldableWithIndex1<URI, number>["reduceWithIndex"] = (
+  fa,
+  b,
+  f,
+) => {
+  // eslint-disable-next-line functional/no-let
+  let i = 0
+  // eslint-disable-next-line functional/no-let
+  let m = b
+  // eslint-disable-next-line functional/no-loop-statement
+  for (const a of fa) {
+    // eslint-disable-next-line functional/no-expression-statement
+    m = f(i++, m, a)
+  }
+  return m
+}
+
+const _foldMapWithIndex: FoldableWithIndex1<URI, number>["foldMapWithIndex"] =
+  M => (fa, f) =>
+    _reduceWithIndex(fa, M.empty, (i, b, a) => M.concat(b, f(i, a)))
+
+export const FoldableWithIndex: FoldableWithIndex1<URI, number> = {
+  URI,
+  foldMapWithIndex: _foldMapWithIndex,
+  foldMap: M => (fa, f) => _foldMapWithIndex(M)(fa, (i, a) => f(a)),
+  reduceWithIndex: _reduceWithIndex,
+  reduce: (fa, b, f) => _reduceWithIndex(fa, b, (i, b, a) => f(b, a)),
+  reduceRight: (fa, b, f) => pipe(fa, toReadonlyArray, A.reduceRight(b, f)),
+  reduceRightWithIndex: (fa, b, f) =>
+    pipe(fa, toReadonlyArray, A.reduceRightWithIndex(b, f)),
 }
--- test/Iterable.ts
+++ test/Iterable.ts
@@ -1,28 +1,96 @@
 import { iterable } from "../src"
-import { string as String } from "fp-ts"
+import { readonlyArray as A, string as String } from "fp-ts"
 import * as fc from "fast-check"
 import * as laws from "fp-ts-laws"
+import { pipe, tuple } from "fp-ts/lib/function"
 
 describe("iterable", () => {
+  describe("destructors", () => {
+    it("toReadonlyArray", () => {
+      fc.assert(
+        fc.property(fc.array(fc.integer()), ix => {
+          expect(
+            iterable.toReadonlyArray(iterable.fromIterable(ix)),
+          ).toStrictEqual(ix)
+        }),
+      )
+    })
+  })
+
   describe("typeclasses", () => {
-    describe("getEq", () => {
-      it("should equal itself when empty", () => {
+    describe("Eq", () => {
+      it("getEq", () => {
         const eq = iterable.getEq(String.Eq)
         // eslint-disable-next-line
         laws.eq(eq, fc.array(fc.string()))
       })
     })
-  })
 
-  describe("Pointed", () => {
-    it("of", () => {
-      const result = []
-      //eslint-disable-next-line functional/no-loop-statement
-      for (const a of iterable.of(1)) {
-        //eslint-disable-next-line
-        result.push(a)
-      }
-      expect(result).toStrictEqual([1])
+    describe("Apply", () => {
+      it("laws", () => {
+        laws.apply(iterable.Apply)(a => a.map(iterable.of), iterable.getEq)
+      })
     })
+
+    describe("Applicative", () => {
+      it("laws", () => {
+        laws.applicative(iterable.Applicative)(
+          a => a.map(iterable.of),
+          iterable.getEq,
+        )
+      })
+    })
+
+    describe("Chain", () => {
+      it("laws", () => {
+        laws.monad(iterable.Monad)(iterable.getEq)
+      })
+    })
+
+    describe("Pointed", () => {
+      it("of", () => {
+        fc.assert(
+          fc.property(fc.integer(), i => {
+            const result = pipe(iterable.of(i), iterable.toReadonlyArray)
+            expect(result).toStrictEqual([i])
+          }),
+        )
+      })
+    })
+
+    describe("FunctorWithIndex", () => {
+      it("laws", () => {
+        laws.functor(iterable.FunctorWithIndex)(
+          arbitrary => arbitrary.map(a => iterable.of(a)),
+          e => iterable.getEq(e),
+        )
+      })
+
+      // this for code coverage, which I can't seem to figure out how to ignore
+      it("mapWithIndex", () => {
+        fc.assert(
+          fc.property(fc.array(fc.integer()), ix => {
+            const result = pipe(
+              iterable.FunctorWithIndex.mapWithIndex(
+                iterable.fromIterable(ix),
+                (i, a) => tuple(a, i),
+              ),
+              iterable.toReadonlyArray,
+            )
+
+            const expected = pipe(
+              ix,
+              A.mapWithIndex((i, a) => tuple(a, i)),
+            )
+
+            expect(result).toStrictEqual(expected)
+          }),
+        )
+      })
+    })
+
+    describe("Compactable", () => {})
+    describe("FilterableWithIndex", () => {})
+    describe("FoldableWithIndex", () => {})
   })
 })
