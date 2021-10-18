4a42e9ee8a4838ce8d46edfb7e61f4718f8858a4
Wayne Van Son <waynevanson@gmail.com>
2021-10-18 19:17:45 +1100

    take and skip combinators

--- src/Iterable.ts
+++ src/Iterable.ts
@@ -2,6 +2,7 @@
  * @since 0.12.0
  */
 import { either as E, eq as EQ, option as O, readonlyArray as A } from "fp-ts"
+import { Alt1 } from "fp-ts/lib/Alt"
 import { Applicative1 } from "fp-ts/lib/Applicative"
 import { Apply1 } from "fp-ts/lib/Apply"
 import { Chain1 } from "fp-ts/lib/Chain"
@@ -16,6 +17,9 @@ import { pipe } from "fp-ts/lib/function"
 import { FunctorWithIndex1 } from "fp-ts/lib/FunctorWithIndex"
 import { Monad1 } from "fp-ts/lib/Monad"
 import { Pointed1 } from "fp-ts/lib/Pointed"
+import { Predicate } from "fp-ts/lib/Predicate"
+import { Refinement } from "fp-ts/lib/Refinement"
+import { Zero1 } from "fp-ts/lib/Zero"
 
 /**
  * @category Model
@@ -147,6 +151,10 @@ export const FunctorWithIndex: FunctorWithIndex1<URI, number> = {
   map: (fa, f) => map(f)(fa),
 }
 
+/**
+ * @category instances
+ * @since 2.7.0
+ */
 export const Apply: Apply1<URI> = {
   ...FunctorWithIndex,
   ap: (fab, fa) => ({
@@ -163,11 +171,19 @@ export const Apply: Apply1<URI> = {
   }),
 }
 
+/**
+ * @category instances
+ * @since 2.7.0
+ */
 export const Applicative: Applicative1<URI> = {
   ...Apply,
   ...Pointed,
 }
 
+/**
+ * @category instances
+ * @since 2.7.0
+ */
 export const Chain: Chain1<URI> = {
   ...Apply,
   chain: (fa, f) => ({
@@ -184,9 +200,42 @@ export const Chain: Chain1<URI> = {
   }),
 }
 
+/**
+ * @category instances
+ * @since 2.7.0
+ */
 export const Monad: Monad1<URI> = {
   ...Pointed,
   ...Chain,
+}
+
+export const Alt: Alt1<URI> = {
+  ...FunctorWithIndex,
+  alt: (fa, that) => ({
+    *[Symbol.iterator]() {
+      // eslint-disable-next-line functional/no-loop-statement
+      for (const a of fa) {
+        //eslint-disable-next-line functional/no-expression-statement
+        yield a
+      }
+
+      // eslint-disable-next-line functional/no-loop-statement
+      for (const a of that()) {
+        //eslint-disable-next-line functional/no-expression-statement
+        yield a
+      }
+    },
+  }),
+}
+
+/**
+ * @category instances
+ * @since 2.7.0
+ */
+export const Zero: Zero1<URI> = {
+  URI,
+  /* eslint-disable-next-line @typescript-eslint/no-empty-function */
+  zero: () => ({ *[Symbol.iterator]() {} }),
 }
 
 const _filterMapWithIndex: FilterableWithIndex1<
@@ -216,12 +265,20 @@ const _partitionMapWithIndex: FilterableWithIndex1<
   right: _filterMapWithIndex(fa, (i, a) => pipe(f(i, a), O.fromEither)),
 })
 
+/**
+ * @category instances
+ * @since 2.7.0
+ */
 export const Compactable: Compactable1<URI> = {
   URI,
   compact: fa => _filterMapWithIndex(fa, (i, a) => a),
   separate: fa => _partitionMapWithIndex(fa, (i, a) => a),
 }
 
+/**
+ * @category instances
+ * @since 2.7.0
+ */
 export const FilterableWithIndex: FilterableWithIndex1<URI, number> = {
   ...Compactable,
   ...FunctorWithIndex,
@@ -298,6 +355,10 @@ const _foldMapWithIndex: FoldableWithIndex1<URI, number>["foldMapWithIndex"] =
   M => (fa, f) =>
     _reduceWithIndex(fa, M.empty, (i, b, a) => M.concat(b, f(i, a)))
 
+/**
+ * @category instances
+ * @since 2.7.0
+ */
 export const FoldableWithIndex: FoldableWithIndex1<URI, number> = {
   URI,
   foldMapWithIndex: _foldMapWithIndex,
@@ -309,15 +370,26 @@ export const FoldableWithIndex: FoldableWithIndex1<URI, number> = {
     pipe(fa, toReadonlyArray, A.reduceRightWithIndex(b, f)),
 }
 
+/**
+ * Takes a function returning an `Option` and keeps it until it contains `None`.
+ * The rest are removed.
+ *
+ * @category Combinators
+ * @since 0.12.0
+ */
 export function takeWhileMapWithIndex<A, B extends A>(
   f: (i: number, a: A) => O.Option<B>,
 ) {
   return (fa: Iterable<A>): Iterable<B> => ({
     *[Symbol.iterator]() {
+      // eslint-disable-next-line functional/no-let
       let i = 0
+      // eslint-disable-next-line functional/no-loop-statement
       for (const a of fa) {
         const ob = f(i++, a)
+        // eslint-disable-next-line functional/no-conditional-statement
         if (O.isSome(ob)) {
+          // eslint-disable-next-line functional/no-expression-statement
           yield ob.value
         } else {
           break
@@ -327,18 +399,94 @@ export function takeWhileMapWithIndex<A, B extends A>(
   })
 }
 
+/**
+ * Takes a predicate and keeps each element until it returns false.
+ * The rest are removed.
+ *
+ * @category Combinators
+ * @since 0.12.0
+ */
+export function takeWhileWithIndex<A, B extends A>(
+  f: PredicateWithIndex<number, A> | RefinementWithIndex<number, A, B>,
+): (fa: Iterable<A>) => Iterable<B> {
+  return takeWhileMapWithIndex((i, a) =>
+    pipe(
+      a,
+      O.fromPredicate((a): a is B => f(i, a)),
+    ),
+  )
+}
+
+/**
+ * Takes a predicate and keeps each element until it returns false.
+ * The rest are removed.
+ *
+ * @category Combinators
+ * @since 0.12.0
+ */
+export function takeWhile<A, B extends A>(
+  f: Predicate<A> | Refinement<A, B>,
+): (fa: Iterable<A>) => Iterable<B> {
+  return takeWhileWithIndex<A, B>((i, a) => f(a))
+}
+
+/**
+ * Takes up to `size` elements and removes the rest.
+ *
+ * @category Combinators
+ * @since 0.12.0
+ */
+export function take(size: number): <A>(fa: Iterable<A>) => Iterable<A> {
+  return takeWhileWithIndex(i => i < size)
+}
+
+/**
+ * Takes a predicate and skips each element until it returns false.
+ *
+ * @category Combinators
+ * @since 0.12.0
+ */
 export function skipWhileWithIndex<A>(f: PredicateWithIndex<number, A>) {
   return (fa: Iterable<A>): Iterable<A> => ({
     *[Symbol.iterator]() {
+      // eslint-disable-next-line functional/no-let
       let i = 0
+      // eslint-disable-next-line functional/no-let
       let take = false
+      // eslint-disable-next-line functional/no-loop-statement
       for (const a of fa) {
+        // eslint-disable-next-line functional/no-conditional-statement
         if (take) {
+          // eslint-disable-next-line functional/no-expression-statement
           yield a
+          // eslint-disable-next-line functional/no-conditional-statement
         } else if (!f(i++, a)) {
+          // eslint-disable-next-line functional/no-expression-statement
           take = true
         }
       }
     },
   })
+}
+
+/**
+ * Takes a predicate and skips each element until it returns false.
+ *
+ * @category Combinators
+ * @since 0.12.0
+ */
+export function skipWhile<A>(
+  f: Predicate<A>,
+): (fa: Iterable<A>) => Iterable<A> {
+  return skipWhileWithIndex((i, a) => f(a))
+}
+
+/**
+ * Skips up to `size` elements and keeps the rest.
+ *
+ * @category Combinators
+ * @since 0.12.0
+ */
+export function skip(count: number): <A>(fa: Iterable<A>) => Iterable<A> {
+  return skipWhileWithIndex(i => i < count)
 }
