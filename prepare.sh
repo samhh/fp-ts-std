074c4eeda6c9edc639430b5c0b6d7a842c88c6aa
Sam A. Horvath-Hunt <hello@samhh.com>
2021-11-24 22:52:48 +0000

    Don't output module type manifests
    The ESM one seemingly isn't needed for tree shaking and caused Webpack et al to treat it as ESM proper... a bit confusing, but not needed for tree shaking which is what we care about.

--- prepare.sh
+++ prepare.sh
@@ -2,16 +2,3 @@
 
 rm -rf dist
 mkdir -p dist/esm dist/cjs
-
-cat > dist/esm/package.json << EOF
-{
-  "type": "module"
-}
-EOF
-
-cat > dist/cjs/package.json << EOF
-{
-  "type": "commonjs"
-}
-EOF
-
