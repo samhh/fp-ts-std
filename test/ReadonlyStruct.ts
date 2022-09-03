9f21bb989fb933b65f115da7eeb02a69a74744df
Sam A. Horvath-Hunt <hello@samhh.com>
2022-09-03 16:20:18 +0100

    Consistency

--- test/ReadonlyStruct.ts
+++ test/ReadonlyStruct.ts
@@ -9,24 +9,26 @@ import { pipe } from "fp-ts/function"
 
 describe("ReadonlyStruct", () => {
   describe("pick", () => {
+    const f = pick
+
     type Thing = { name: string; age: number; email?: string }
     const x: Thing = { name: "Hodor", age: 123 }
     const y: Thing = { name: "Hodor", age: 123, email: "foo@bar.com" }
 
     it("picks no keys", () => {
-      expect(pipe(x, pick([]))).toEqual({})
+      expect(pipe(x, f([]))).toEqual({})
     })
 
     it("picks individual keys", () => {
-      expect(pipe(x, pick(["name"]))).toEqual({ name: "Hodor" })
-      expect(pipe(x, pick(["age"]))).toEqual({ age: 123 })
-      expect(pipe(x, pick(["email"]))).toStrictEqual({})
-      expect(pipe(y, pick(["email"]))).toStrictEqual({ email: "foo@bar.com" })
+      expect(pipe(x, f(["name"]))).toEqual({ name: "Hodor" })
+      expect(pipe(x, f(["age"]))).toEqual({ age: 123 })
+      expect(pipe(x, f(["email"]))).toStrictEqual({})
+      expect(pipe(y, f(["email"]))).toStrictEqual({ email: "foo@bar.com" })
     })
 
     it("picks multiple keys", () => {
-      expect(pipe(x, pick(["name", "age", "email"]))).toStrictEqual(x)
-      expect(pipe(y, pick(["name", "age", "email"]))).toStrictEqual(y)
+      expect(pipe(x, f(["name", "age", "email"]))).toStrictEqual(x)
+      expect(pipe(y, f(["name", "age", "email"]))).toStrictEqual(y)
     })
   })
 
@@ -50,20 +52,22 @@ describe("ReadonlyStruct", () => {
   })
 
   describe("omit", () => {
+    const f = omit
+
     type Thing = { name: string; id: string; foo: string }
 
     it("omits multiple keys", () => {
       const before: Thing = { name: "Ragnor", id: "789", foo: "Bar" }
       const after: Omit<Thing, "id" | "foo"> = { name: "Ragnor" }
 
-      expect(omit(["id", "foo"])(before)).toEqual(after)
+      expect(f(["id", "foo"])(before)).toEqual(after)
     })
 
     it("typechecks missing keys", () => {
       const before: Thing = { name: "Ragnor", id: "789", foo: "Bar" }
       const after: Omit<Thing, "id"> = { name: "Ragnor", foo: "Bar" }
 
-      expect(omit(["id", "bar"])(before)).toEqual(after)
+      expect(f(["id", "bar"])(before)).toEqual(after)
     })
   })
 
@@ -80,23 +84,25 @@ describe("ReadonlyStruct", () => {
   })
 
   describe("withDefaults", () => {
+    const f = withDefaults
+
     it("provides defaults for optional values, requiring values if and only if a property is optional", () => {
       const x: { a: number; b?: string } = { a: 1 }
-      const y = pipe(x, withDefaults({ b: "foo" }))
+      const y = pipe(x, f({ b: "foo" }))
 
       // @ts-expect-error -- missing an optional property of x
-      pipe(x, withDefaults({}))
+      pipe(x, f({}))
       // @ts-expect-error -- includes required properties
-      pipe(x, withDefaults({ a: 2, b: "" }))
+      pipe(x, f({ a: 2, b: "" }))
       // @ts-expect-error -- includes excess properties
-      pipe(x, withDefaults({ b: "", c: "foo" }))
+      pipe(x, f({ b: "", c: "foo" }))
 
       expect(y).toEqual({ a: 1, b: "foo" })
     })
 
     it("preserves preexisting optional properties", () => {
       const x: { a: number; b?: string } = { a: 1, b: "foo" }
-      const y = pipe(x, withDefaults({ b: "bar" }))
+      const y = pipe(x, f({ b: "bar" }))
 
       expect(y).toEqual(x)
     })
