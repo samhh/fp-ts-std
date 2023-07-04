64e53980a4cb3250a5929486a6f9e2c128b8d7b8
Sam A. Horvath-Hunt <hello@samhh.com>
2023-07-04 17:11:01 +0100

    Access docs script from npm script

--- docs.sh
+++ docs.sh
@@ -12,7 +12,7 @@ sd '"type": "module"' '"type": "commonjs"' ./docs-workdir/package.json
 sd '"moduleResolution": "node16"' '"moduleResolution": "node"' ./docs-workdir/tsconfig.json
 sd 'from "(.+).js"$' 'from "$1"' ./docs-workdir/src/*.ts
 
-(cd ./docs-workdir/; yarn docs)
+(cd ./docs-workdir/; yarn run docs-ts)
 
 rm -rf ./docs/
 mv ./docs-workdir/docs/ ./docs
