f5c6931768f9d90f2d48826985bc3a08e8236fb5
Sam A. Horvath-Hunt <hello@samhh.com>
2023-10-24 15:54:29 +0100

    eslint-plugin-functional 6.x

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     "functional/prefer-tacit": 2,
     "functional/no-expression-statements": [
       2,
-      { ignorePattern: "(describe)|(it)|(expect)|(fc.)|(laws.)" },
+      { ignoreCodePattern: "(describe)|(it)|(expect)|(fc.)|(laws.)" },
     ],
     // Tripped in Jest suites
     "functional/no-return-void": [2, { ignoreInferredTypes: true }],
