d9cede1c89d2522dc7ad2c84bac099277492b913
Sam A. Horvath-Hunt <hello@samhh.com>
2023-10-24 15:10:46 +0100

    Support x86_64-darwin too

--- tshm-docs-ts.nix
+++ tshm-docs-ts.nix
@@ -1,15 +1,18 @@
-# This is a bit lazy at the moment, supporting only Linux x86_64.
-
 { fetchurl, lib, stdenv }:
 
 stdenv.mkDerivation rec {
   pname = "tshm-docs-ts";
   version = "0.4.2";
 
-  src = fetchurl {
-    url = "https://github.com/samhh/tshm/releases/download/${version}/tshm-docs-ts-${version}-linux-x86_64";
-    sha256 = "128skc68bkcfc0bg0xjwd9dn9203by70dzvr6ki9nhr3qvggnf2p";
-  };
+  src = if stdenv.isDarwin
+    then fetchurl {
+      url = "https://github.com/samhh/tshm/releases/download/${version}/tshm-docs-ts-${version}-macos-x86_64";
+      sha256 = "1hmxz6sslxp6336dacc18fbg425bxabvnkassyg7z111hk6jzz74";
+    }
+    else fetchurl {
+      url = "https://github.com/samhh/tshm/releases/download/${version}/tshm-docs-ts-${version}-linux-x86_64";
+      sha256 = "128skc68bkcfc0bg0xjwd9dn9203by70dzvr6ki9nhr3qvggnf2p";
+    };
 
   dontUnpack = true;
   dontStrip = true;
@@ -23,6 +26,6 @@ stdenv.mkDerivation rec {
   meta = {
     homepage = "https://github.com/samhh/tshm";
     license = lib.licenses.mit;
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "x86_64-linux" "x86_64-darwin" ];
   };
 }
