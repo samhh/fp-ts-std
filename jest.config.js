7f1cb894c33d45f1aadd401e5787292b3719417c
Sam A. Horvath-Hunt <hello@samhh.com>
2024-04-19 13:18:41 +0100

    Revert "Hopefully back to 100% Jest coverage"
    This reverts commit 37305b46fc40fc659eda9535d4ef013222c3559c.
    
    Nope, still not quite there on Linux latest Node.js. 🙃

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ module.exports = {
 	coverageProvider: "v8",
 	coverageThreshold: {
 		global: {
-			branches: 100,
+			// Principally 100%, but coverage is reported inconsistently across
+			// platforms, so we'll give it a little wiggle room here.
+			branches: 99,
 			functions: 100,
 			lines: 100,
 			statements: 100,
